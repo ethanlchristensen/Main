858a8e44595374e3dab646cc3485c1bcf111eba3
Ethan Christensen <ethanlchristensen@outlook.com>
2022-01-05 04:57:57 -0600

    More Leet Code Problems

---
+++ Python/LeetCodeProblems/EASY/LeetCode100.py
@@ -0,0 +1,52 @@
+# LEET CODE 100 Same Tree
+# Given the roots of two binary trees p and q, write
+# a function to check if they are the same or not . . .
+from BinaryTree import BinaryNode
+
+def isSameTree(p, q): # solution one
+    def process(node):
+        res = []
+        def helper(node):
+            if node == None:
+                res.append("NULL")
+                return
+            res.append(node.val)
+            helper(node.left)
+            helper(node.right)
+        helper(node)
+        return res
+    tree1 = process(p)
+    tree2 = process(q)
+    return tree1 == tree2
+
+def isSameTreeV2(p, q): # updated solution
+    def helper(n1, n2):
+        if n1 == None and n2 == None:
+            return True
+        if n1 == None and n2 != None:
+            return False
+        if n1 != None and n2 == None:
+            return False
+        if n1.val == n2.val:
+            return helper(n1.left, n2.left) and helper(n1.right, n2.right)
+        else:
+            return False
+    res = helper(p, q)
+    return res
+    
+        
+    
+
+
+
+t1 = BinaryNode(1, BinaryNode(2), BinaryNode(3))
+t2 = BinaryNode(1, BinaryNode(2), BinaryNode(3))
+t3 = BinaryNode(1, BinaryNode(2), BinaryNode(9))
+t4 = BinaryNode(1, BinaryNode(2), None)
+t5 = BinaryNode(1, None, BinaryNode(2))
+print(isSameTree(t1, t2))
+print(isSameTree(t2, t3))
+print(isSameTree(t4, t5))
+print(isSameTreeV2(t1, t2))
+print(isSameTreeV2(t2, t3))
+print(isSameTreeV2(t4, t5))
--- Python/LeetCodeProblems/EASY/LeetCode66.py
+++ Python/LeetCodeProblems/EASY/LeetCode66.py
@@ -3,19 +3,75 @@
 # return the result when one is added to the intger as a list
 
 def plusOne(digits):
-    for i in range(len(digits)-1,-1,-1):
+    carry = 0
+    completed = False
+    for i in range(len(digits) - 1, -1, -1):
+        if completed:
+            break
         if digits[i] == 9:
-            digits[i] == 0
+            digits[i] = 0
+            if len(digits) == 1:
+                carry += 1
+                break
             for j in range(i-1, -1, -1):
-                if digits[j] < 9:
+                if digits[j] == 9:
+                    digits[j] = 0
+                else:
                     digits[j] += 1
+                    completed = True
                     break
-                digits[j] = 0
-        digits[i] += 1
-        break
+                if j == 0:
+                    carry += 1
+            completed = True
+        else:
+            digits[i] += 1
+            break
+    if carry > 0:
+        return [1] + digits
+    else:
+        return digits
+
+def plusOneV2(digits):
+    digits = int(''.join([str(d) for d in digits]))
+    digits += 1
+    res = []
+    while digits != 0:
+        res.append(digits%10)
+        digits = digits // 10
+    res = res[::-1]
+    return res
+
+
+def plusOnev3(digits):
+    noCarry = False
+    p = len(digits) - 1
+    while p > -1:
+        if digits[p] < 9:
+            digits[p] += 1
+            noCarry = True
+            break
+        else:
+            digits[p] = 0
+            p -= 1
+    if noCarry:
+        return digits
+    else:
+        return [1] + digits
+        
+    
     
 
 
-num = [1,2,3]
-plusOne(num)
-print(num)
+test_cases = [[1,2,3],
+              [9,9,9],
+              [9,9],
+              [9],
+              [4,3,2,2],
+              [4,3,2,1]]
+for case in test_cases:
+    print("{} + 1 -> {}".format(case, plusOne(list(case))))
+for case in test_cases:
+    print(plusOneV2(case))
+
+num = [1,2,9]
+print(plusOnev3(num))
